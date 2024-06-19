8b3fb701e8a03417993aade11f741d783dc84fd5
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2024-06-19 16:49:45 +0900

    デモサイト用設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          100: '#ebf8ff',
-          300: '#90cdf4',
-          500: '#4299e1',
-        },
-      },
+      colors: { body: '#050505' },
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
     },
   },
   plugins: [],

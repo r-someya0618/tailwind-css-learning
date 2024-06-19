ad48db263c5e9eaedc90464bdc63e6902c7193f6
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2024-06-19 17:28:20 +0900

    ナビゲーションメニュー設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-      colors: { body: '#050505' },
+      colors: { body: '#050505', 'selected-text': '#A3A3FF', theme: '#5c318c' },
     },
     fontFamily: {
       poppins: ['Poppins', 'sans-serif'],
